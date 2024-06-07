fcac359b76691e7bee83669d3fc666f7a222b599
GProgrammerN <guilherme.programador12@gmail.com>
2024-06-07 15:21:52 -0300

    Altered text

--- app/components/MainContent.tsx
+++ app/components/MainContent.tsx
@@ -4,7 +4,7 @@ export default function MainContent() {
     return (
         <main>
             <span className="leading-normal max-w-xs md:max-w-none">
-                <p>Sempre tive interesse na área da tecnologia, mexendo com criação de servidores de jogos durante a adolescência. Cheguei a fazer alguns sobre Python e a tríade web na época do ensino médio, acabando não dando sequência.</p>
+                <p>Sempre tive interesse na área da tecnologia, mexendo com criação de servidores de jogos durante a adolescência. Cheguei a fazer alguns sobre Python e a tríade web na época do ensino médio.</p>
                 <p className="mt-4 mb-16">
                     Mas foi em 2023 que, meu pai, ex-programador (atualmente advogado) teve a ideia de iniciar o projeto de um <a href="https://sistemaexecucaopenal.vercel.app/" target="_blank" rel="noreferrer" className="font-bold text-white hover:text-teal-300 focus-visible:text-teal-300">micro SaaS (Software como Serviço)</a> voltado para advogados criminalistas. E foi a partir desse projeto que comecei a evoluir muito como programador. Desde então, venho focando todos os meus esforços para me aprimorar na área.
                 </p>
